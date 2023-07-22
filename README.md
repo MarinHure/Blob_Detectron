c0fcee658e1eab5f38112bfb5be2d47618d42bfd
Marin Hure <126484240+dodouce31@users.noreply.github.com>
2023-07-22 12:18:05 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,192 @@
-# Blob_detectron
+# Blob detectron inference
+
+ Le projet vise à développer un outil pour détecter et simuler automatiquement le réseau créé par le Physarum polycephalum (blob) à partir d'une image d'entrée. 
+
+Le CNRS réalise des expériences avec le blob dans des boîtes de pétri, et les photographies. 
+
+![Physarum boite de petri](images_readme/boitepetri.png) 
+
+
+Pour ce faire, nous déclarons 3 classes différentes :  
+
+    - Les nœuds principaux (Np) : Point d’attirance, souvent un flocon d’avoine.  
+
+    - Les liens (Lp) : Le chemin parcouru par le blob entre deux nœuds. 
+
+    - Les nœuds secondaires (Ns) : croisement entre plusieurs liens. 
+
+Afin de détecter automatique les différents éléments sur une image, nous utilisons un modèle de machine learning de détection d’objets. 
+
+Nous exploitons ensuite les données identifiées dans un script afin d’obtenir un fichier JSON contenant les informations de position des différents objets.  
+
+# Entrainement du modèle (Blob_training.ipynb) 
+
+Le dataset est composé de 192 images de boite de pétri, annotées avec l’outil Supervisely. 
+
+![Annotation avec supervisely](images_readme/supervisely.png)  
+
+Les annotations sont exportées au format coco, et séparées en données d’entrainement "physarum_train" (80%) et de test "physarum_test" (20%). 
+
+Le choix du modèle s’est porté sur Mask R-CNN, une extension de Faster RCNN, un modèle utilisant l’API de Detectron2. 
+
+L'entrainement du modèle se fait sur le notebook Blob_training.ipynb
+
+Faster R-CNN est un réseau de neurones, il fonctionne sur deux étages, d’abord il identifie les zones d’intéret, puis les envoie a un réseau neuronal convolutif. Les résultats de sortie sont passés à une Machine à vecteurs de support (SVM)  pour les classifier. 
+
+Une régression est ensuite effectuée pour ajuster la boîte englobante prédite à la position et à la taille exactes de l'objet réel dans la région d'intérêt. 
+
+Mask R-CNN est capable de réaliser à la fois la détection d'objets et la segmentation sémantique des instances détectées, ce qui nous permet d’obtenir des masques des objets détectés, plutôt que de simples box. 
+
+## Réglages d’entrainement du modèle :  
+
+Pour accélérer l'entraînement et améliorer les performances du modèle, le poids initial du réseau est chargé à partir du modèle pré-entraîné "R-50.pkl" sur ImageNet. 
+
+cfg.SOLVER.IMS_PER_BATCH = 4 définit le nombre d'images utilisées pour chaque mise à jour de gradient. Une taille de lot plus grande permet d'exploiter davantage les ressources GPU, accélérant ainsi l'entraînement. 
+
+Le modèle est entraîné pendant cfg.SOLVER.MAX_ITER = 3000 itérations. Le nombre d'itérations contrôle la durée de l'entraînement et permet au modèle de converger vers une solution optimale. 
+
+Taille du lot par image : Le paramètre cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 8 spécifie le nombre de propositions de région (ROI) pour chaque image d'entraînement. 
+
+Seuil de score de détection : Le seuil de score de détection est fixé à cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.45. Cela signifie que seules les détections dont le score est supérieur à 0.45 seront prises en compte lors de l'inférence. 
+
+## Mesures de performance du modèle
+
+Pour mesurer les performances du modèle, on visualise certaines métriques de performance :
+
+![Métriques de performance](images_readme/evaluation.png)
+
+On évalue la performance du modèle :  
+- Average Precision : en comparant ses prédictions avec les annotations réelles à différentes valeurs seuil d'Intersection over Union (IoU : mesure le degré de chevauchement entre la prédiction du modèle et l'annotation réelle d'un objet).
+- Average Recall : indique quelle proportion des objets réellement présents dans l'image a été correctement détectée par le modèle.
+
+On visualise aussi les résultats pour mesurer les performances au fur et a mesure des réglages : 
+
+![Visualisation Résultats](images_readme/visualisation1.png)  ![Visualisation NetworkX](images_readme/visualisation2.png)
+
+
+# Fonctionnement du script :  
+
+Le script Blob_detection.py prends une image en entrée et donne en sortie un fichier Json contenant les positions des objets prédits. 
+
+Pour ce faire, le script est composé de 6 fonctions :  
+
+## prediction(img_name, data_dir): 
+
+Cette fonction effectue une prédiction des objets présents sur l’image, et donne en sortie les informations à leur sujet.  
+
+Cette prédiction s’effectue à l’aide du modèle pré-entrainé, enregistré en modèle Torchscript. 
+
+Params:  
+    
+    - img_name : nom du fichier d el’image à détecter. 
+
+    - data_dir : chemin du dossier ou se trouvent le script. 
+
+Returns: 
+
+    - outputs : variable qui contient les informations des prédictions. 
+
+    - img : l’image d’entrée. 
+        
+Dans le processus, l’image est transformée de RGB (Red Green Blue) à BGR pour que le modèle fonctionne correctement.  
+
+## extractionOutputs(outputs): 
+
+Cette fonction extrait les données des ojbets prédits et les places dans des listes. 
+
+Params :  
+    
+    - outputs : variable qui contient les informations des prédictions. 
+
+Returns : 
+   
+    - nœuds : liste des nœuds détectés. 
+
+    - positions : dictionnaire contenant les positions (x,y) de chaque nœud. 
+
+    - positionLiens : dictionnaire contenant les positions (x,y) de chaque lien. 
+
+La fonction parcourt la variable output, prend le premier tensor (output[0][i]) qui contient les box de prédictions. 
+
+On prend le centre de chaque boite pour identifier la position des objets.  
+
+On vérifie la classe prédite (pred_class) afin de placer l’objet dans la bonne liste. 
+
+Pour les nœuds secondaires, on vérifie qu’aucun autre nœud ne se situe à moins de 6, car il arrive au modèle de détecter deux nœuds secondaires au même endroit. 
+
+## detectionLiens(positions, positionLiens, seuil, marge): 
+
+Cette fonction détecte si un lien est présent entre deux nœuds à l’aide des listes précédemment produites. 
+
+Params : 
+   
+    - positions : dictionnaire contenant les positions (x,y) de chaque nœud. 
+
+    - positionLiens : dictionnaire contenant les positions (x,y) de chaque lien. 
+
+Returns :  
+   
+    - listeLiensFinal : liste des liens détectés entre deux nœuds, composés de tuples de nœuds. 
+
+Une double boucle parcourt la liste de nœuds, trace une droite entre chaque paire de nœuds, puis parcourt la liste de liens. On calcule la distance entre le lien et la droite, si elle est inférieure au “seuil”, on considère que le lien est prédsent entre deux nœuds.  
+
+De plus, on vérifie que les x et y du lien, sont placés entre les x et y des nœuds (avec une “marge”). 
+
+Ensuite, on parcourt nos résultats, pour chaque paire de nœuds reliée par un lien, on vérifie qu’il n’existe pas une paire de nœuds plus rapprochés.  
+
+## drawGraph(nœuds, positions, listeLiensFinal): 
+
+Cette fonction crée un graphe NetworkX à partir des listes de nœuds et de liens. 
+
+Params : 
+   
+    - nœuds:  liste des nœuds. 
+
+    - positions : dictionnaire contenant les positions (x,y) de chaque nœud. 
+
+    - listeLiensFinal: Liste des paires de nœuds connectés par des liens. 
+
+## conversionBlobRecorder(image,graph,positions,echelle,img_name): 
+
+Cette fonction converti le graphe NetworkX en JSON exploitable par l’outil Blob Recorder. 
+
+Params :  
+   
+    - image: c’est l’image d’origine. 
+
+    - graph: le graphe NetworkX. 
+
+    - positions: le dictionnaire contennant les positions des différents nœuds. 
+
+    - echelle: une echelle pour convenir au format d’image de Blob Recorder.  
+
+    - img_name: le nom de l’image 
+
+La fonction parcourt les listes de nœuds et liens, pour rentrer les valeurs dans un dictionnaire au format attendu par Blob Recorder. 
+
+Ensuite elle enregistre le résultat dans un fichier JSON au même nom que l’image. 
+
+## blobDetection(img_name, data_dir): 
+
+
+Cette dernière fonction permet de lancer les autres fonctions.  
+
+
+Params : 
+   
+    - img_name : le nom de l’image à détecter. 
+    
+    - data_dir : le chemin du répertoire ou se trouvent les fichiers. 
+
+ 
+
+# Limites/Problèmes rencontrés
+
+Quelques fois, le blob trace un lien avec une trop forte courbure, et la fonction detectionLiens() utilise son centre pour détecter sa présence, ce qui peut amener à des erreurs, c'est à ca que servent "seuil" et "marge", mais cela ne suffit pas toujours : 
+
+![Erreur detection lien](images_readme/pb_lien.png)
+
+Il arrive que le modèle détecte 2 Noeuds secondaires collés l'un à l'autre au lieu d'un seul. Ce problème a été réglé en post-traitement dans extractionOutputs() : 
+
+![Erreur detection Ns](images_readme/pb_ns.png)
+
---
+++ blob_detectron_inference.py
@@ -0,0 +1,247 @@
+# !pip install torch torchvision -f https://download.pytorch.org/whl/torch_stable.html
+# !pip install git+https://github.com/facebookresearch/fvcore.git
+
+import torch
+import torchvision
+import os
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+from matplotlib.offsetbox import bbox_artist
+import math
+import networkx as nx
+import json
+import matplotlib.pyplot as plt
+
+def prediction(img_name, data_dir):
+  """
+  This function predicts objects in an image, using a pretrained torchscript model.
+
+  :param img_name: This is the name of the image file
+  :param data_dir: The path of the directory
+
+  :returns: Returns the predictions and the image (numpy array)
+  """
+  SEP = os.path.sep
+  tst_imgs = os.path.join(data_dir, "test_images")  # Chemin du dossier des images pour tester
+  model_dir_ts = os.path.join(data_dir, "model_ts") # Chemin du dossier ou est stocké le modèle
+  model_path_ts = os.path.join(model_dir_ts, "detectron2_physarum_complet.pt")
+  img_test = os.path.join(tst_imgs, img_name)
+  pil_img = Image.open(img_test) # Charger l'image
+  img = np.array(pil_img)[:,:,::-1] # BGR
+  # loaded_model = torch.jit.load(model_path_ts) # Charger le modèle
+  # _ = loaded_model.to('cpu')
+  loaded_model = torch.load(model_path_ts, map_location=torch.device('cpu'))
+  outputs = loaded_model(torch.as_tensor(img.astype('float32').transpose(2, 0, 1))) # Faire les prédictions
+  return outputs, img
+
+
+def extractionOutputs(outputs):
+  """
+  This function extracts the prediction informations, and put them in arrays.
+
+  :param outputs: This is the output for the predictions
+  :returns: Returns arrays that contains the nodes informations (name, position) and their links (name, position)
+  """
+  noeuds = []
+  positions = {}
+  liens = []
+  positionLiens = {}
+  c= c1= c2=1 # Compteurs (Np, Liens, Ns)
+
+  for i in range(len(outputs[0])):
+    bbox = outputs[0][i]
+    bbox = bbox.detach().cpu().numpy()
+    pred_classes = outputs[1][i]
+    x=((bbox[0]+bbox[2])/2) #  On prend le centre de la boite
+    y=((bbox[1]+bbox[3])/2)
+
+    if pred_classes == 0: # Noeud principal
+      bbox_str = 'Np' + str(c) # Nommage avec compteur
+      c=c+1
+      noeuds.append(bbox_str) # Ajout du nœud à la liste des nœuds
+      positions[bbox_str] = [x,y] 
+
+    elif pred_classes == 1: #Liens
+      bbox_str = 'L' + str(c1)
+      c1=c1+1
+      liens.append(bbox_str)
+      positionLiens[bbox_str] = [x,y]
+
+    elif pred_classes == 2: # Noeud secondaire
+      verif= True
+      bbox_str = 'Ns' + str(c2)
+      for j in positions:
+        if bbox_str!=j:
+          x1=positions[j][0]
+          y1=positions[j][1]
+          distance=math.sqrt((x1-x)**2 + (y1-y)**2)  #  On vérifie qu'il n'y a pas de Ns détectés collés (le modèle fait l'erreur qq fois)
+          if distance<6 :
+             verif=False
+             print(bbox_str,j,distance)
+             break
+      if verif :
+        c2=c2+1
+        noeuds.append(bbox_str)
+        positions[bbox_str] = [x,y]
+  return noeuds, positions, positionLiens
+
+
+def detectionLiens(positions, positionLiens):
+  """
+  This function detects if there is a link between two nodes.
+
+  :param positions: Contains the positions of the nodes.
+  :param positionLiens: Contains the positions of the links.
+  :returns: Returns the list of links, represented by a tuple of nodes.
+  """
+  seuil=25 # Seuil distance lien / droite entre deux noeuds
+  marge=25 # MARGE POINT(lien) COMPRIS ENTRE X1 X2
+  listeLiens = []  # Liste de liens détectés entre deux noeuds (avec distance entre les noeuds)
+  for i in positions:
+    for j in positions:
+      if i!= j:
+        x1= positions[i][0]
+        x2= positions[j][0]
+        y1= positions[i][1]
+        y2= positions[j][1]
+        ecart = math.sqrt((x2-x1)**2 + (y2-y1)**2) #  Distance entre 2 noeuds
+        for k in positionLiens:
+          x= positionLiens[k][0]
+          y= positionLiens[k][1]
+          distance = abs((y2 - y1) * x - (x2 - x1) * y + x2 * y1 - y2 * x1) / math.sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2) # Distance point - ligne
+          if (distance <= seuil) and ((y1-marge <= y <= y2+marge) or (y2-marge<= y <= y1+marge)) and ((x1-marge<= x <=x2+marge) or (x2-marge<= x <= x1+marge)) : # Vérifie si lien présent à distance (seuil) de la droite tracée par deux noeuds
+            listeLiens.append((k,i,j,ecart))
+
+  resultats = {} # On ne garde que les paires de noeuds les plus proches
+  for k, i, j, ecart in listeLiens:
+      if k not in resultats:  # Si lien pas encore dans le dictionnaire
+          resultats[k] = (i, j, ecart)
+      else:
+          _, _, min_ecart = resultats[k] #  Ecart minimum enregistré
+          if ecart < min_ecart: # Comparaison
+              resultats[k] = (i, j, ecart)
+
+  listeLiensFinal = [] # Liste finale pour NetworkX
+  for k in resultats:
+    i, j, _ = resultats[k]
+    listeLiensFinal.append((i,j))
+  return listeLiensFinal
+
+
+def drawGraph(noeuds, positions, listeLiensFinal, image):
+  """
+  This function creates a NetworkX graph from the nodes/link informations and overlay the graph on the base image.
+
+  :param noeuds: This is a list of the existing nodes.
+  :param positions: This is a dictionnary of the coordinates of each node.
+  :param listeLiensFinal: Contains tuples of nodes that are connected by a link.
+  :param image: This is the original image
+  :returns: Rerturns the NetworkX Graph that has been created.
+  """
+  graph = nx.Graph()
+  graph.add_nodes_from(noeuds)
+  graph.add_edges_from(listeLiensFinal)
+  return graph
+
+
+def conversionBlobRecorder(img,graph,positions,echelle,img_name):
+  """
+  This function converts the NetworkX graph to the "Blob Recorder" JSON format and saves it into the JSON_results folder.
+
+  :param img: This is the numpy array image.
+  :param graph: This is the NetworkX graph.
+  :param positions: Contains the coordinates of each node.
+  :param echelle: This is a scaling variable, to fit the Blob Recorder image.
+  :param img_name: This is the name of the image.
+  :returns: Returns a JSON file, with the name of the image.
+  """
+  image_height = img.shape[0] # Hauteur de l'image pour inversion axe y
+  blobRecorder = {          # Dictionnaire stockant données
+      "type": "FeatureCollection",
+      "features": []
+  }
+  # Ajout nœuds
+  for node in graph.nodes:
+      x, y = positions[node]    # On récupère les posisitions
+      y_inverted = image_height - y  # Inverser axe y
+      x_echelle = x * echelle # Mise à l'échelle
+      y_echelle = y_inverted * echelle # Mise à l'échelle
+      if(node.startswith('Np')): # Noeud Principal
+        feature = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [x_echelle, y_echelle]
+            },
+            "properties": {
+                "geometryNamex": "Noeud source",
+                "_cost": 1,
+                "_customName": node
+            }
+          }
+      elif(node.startswith('Ns')):   # Noeud secondaire
+              feature = {
+                  "type": "Feature",
+                  "geometry": {
+                      "type": "Point",
+                      "coordinates": [x_echelle, y_echelle]
+                  },
+                  "properties": {
+                      "geometryNamex": "Noeud secondaire",
+                      "_cost": 1,
+                      "_customName": node
+                  }
+              }
+      blobRecorder["features"].append(feature)
+  # Ajout liens
+  for edge in graph.edges:
+      start_node, end_node = edge   # Noeuds connectés au lien
+      start_x, start_y = positions[start_node]  # x,y premier noeud
+      end_x, end_y = positions[end_node]   # x,y second noeud
+      start_x_echelle = start_x * echelle # mise à l'échelle
+      end_x_echelle = end_x * echelle # mise à l'échelle
+      start_y_inverted = image_height - start_y  # Inversion axe y
+      end_y_inverted = image_height - end_y
+      start_y_echelle = start_y_inverted * echelle # mise à l'échelle
+      end_y_echelle = end_y_inverted * echelle # mise à l'échelle
+      feature = {
+          "type": "Feature",
+          "geometry": {
+              "type": "LineString",
+              "coordinates": [[start_x_echelle, start_y_echelle], [end_x_echelle, end_y_echelle]]
+          },
+          "properties": {
+              "geometryNamex": "Veine primaire",
+              "_cost": 1
+          }
+      }
+      blobRecorder["features"].append(feature)
+
+  file_name = img_name[:-3] + "json"    # Nom de l'image +json
+  script_dir = os.path.dirname(__file__)  # Chemin ou se trouve le script
+  json_dir = os.path.join(script_dir,"JSON_Results") # Chemin du dossier JSON_Results
+  full_path = os.path.join(json_dir, file_name)  # Emplacement du fichier JSON
+  with open(full_path, "w") as outfile: # Ouverture du fichier en écriture
+      json.dump(blobRecorder, outfile)
+  return full_path
+
+
+def blobDetection(img_name, data_dir):
+  """
+  This function predicts objects from a physarum polycephalum image and put the information in a JSON file.
+
+  :param data_dir: This is the path to the directory directory
+  :param img_name: This is the name of the image.
+  """
+  
+  echelle = 1.28 # Mise à l'échelle par rapport a l'image blob recorder
+
+  outputs, image = prediction(img_name, data_dir)  # Prediction using TorchScript Model
+  noeuds, positions, positionLiens = extractionOutputs(outputs)  # Extraction of outputs data
+  listeLiensFinal = detectionLiens(positions, positionLiens)  # Link detection between Nodes
+  graph = drawGraph(noeuds, positions, listeLiensFinal, image)  # Conversion to NetworkX and Drawing
+  file_json = conversionBlobRecorder(image,graph,positions,echelle,img_name)  # Conversion NetworkX to Blob Recorder
+
+blobDetection("boite_41_1.png", "C:\\Users\\Marin HURE\\Desktop\\physarum")
+
