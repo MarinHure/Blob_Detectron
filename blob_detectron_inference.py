70a5642ab131c53e583d1218ae511d6cccbc7fbd
Marin Hure <126484240+MarinHure@users.noreply.github.com>
2023-09-27 17:00:10 -0600

    Update blob_detectron_inference.py

--- blob_detectron_inference.py
+++ blob_detectron_inference.py
@@ -97,7 +97,7 @@ def detectionLiens(positions, linkPositions):
   """
   threshold=25 # threshold distance lien / droite entre deux nodes
   margin=25 # margin POINT(lien) COMPRIS ENTRE X1 X2
-  listeLiens = []  # Liste de liens détectés entre deux nodes (avec distance entre les nodes)
+  linksList = []  # Liste de liens détectés entre deux nodes (avec distance entre les nodes)
   for i in positions:
     for j in positions:
       if i!= j:
@@ -111,10 +111,10 @@ def detectionLiens(positions, linkPositions):
           y= linkPositions[k][1]
           distance = abs((y2 - y1) * x - (x2 - x1) * y + x2 * y1 - y2 * x1) / math.sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2) # Distance point - ligne
           if (distance <= threshold) and ((y1-margin <= y <= y2+margin) or (y2-margin<= y <= y1+margin)) and ((x1-margin<= x <=x2+margin) or (x2-margin<= x <= x1+margin)) : # Vérifie si lien présent à distance (threshold) de la droite tracée par deux nodes
-            listeLiens.append((k,i,j,ecart))
+            linksList.append((k,i,j,ecart))
 
   resultats = {} # On ne garde que les paires de nodes les plus proches
-  for k, i, j, ecart in listeLiens:
+  for k, i, j, ecart in linksList:
       if k not in resultats:  # Si lien pas encore dans le dictionnaire
           resultats[k] = (i, j, ecart)
       else:
